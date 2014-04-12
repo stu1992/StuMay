975a1079719eb0932e8de143b4332fd4934f50df
Stu <stumay1992@gmail.com>
2014-04-12 22:19:57 +1000

    background image transition applied

--- www/buttons.js
+++ www/buttons.js
@@ -7,7 +7,6 @@ url[3]="projects/index.html";
 window.location=url[page];
 }
 
-
 function swapImage(page) {
 var url = new Array();
 url[0]="jpg/about.jpg";
@@ -16,29 +15,14 @@ url[2]="jpg/blog.jpg";
 url[3]="jpg/projects.jpg";
 url[4]="jpg/home.jpg"; // this is for mouseout actions
 // define image locations
-document.getElementById('wrapper').style.backgroundImage = "url("+url[page]+")";
+document.getElementById('t').style.backgroundColor="white";
+setTimeout(function(){document.getElementById('wrapper').style.backgroundImage = "url("+url[page]+")"},500); 
+var newC = document.getElementById('t');
+setTimeout(function(){newC.style.backgroundColor="transparent"},500);
 }
 
-function fadeIn(page) {
-// this gives me the images
-var url = new Array();
-url[0]="jpg/about.jpg";
-url[1]="jpg/skills.jpg";
-url[2]="jpg/blog.jpg";
-url[3]="jpg/projects.jpg";
-url[4]="jpg/home.jpg"; // this is for mouseout actions
-
- document.getElementById('t').style.backgroundImage = document.getElementById('wrapper').style.backgroundImage;
- // will make the table white over time
-}
-function fadeOut(page) {
-document.getElementById('t').style.backgroundColor = "transparent";
-   
-}
 var init;
 function image(page) {
 clearTimeout(init);
-setTimeout(function(){fadeIn(page)}, 1000);
 init = setTimeout(function(){swapImage(page)},1000);
-setTimeout(function a(){fadeOut(page)}, 1000);
 }
