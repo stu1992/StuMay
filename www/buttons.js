a1ef1e58ac04281a6e07ddb05b45d30f98c03888
Stu <stumay1992@gmail.com>
2014-04-12 19:26:30 +1000

    experimenting with swapping pictures I have half the funcitonality but want it to be cleaner

--- www/buttons.js
+++ www/buttons.js
@@ -7,17 +7,29 @@ url[3]="projects/index.html";
 window.location=url[page];
 }
 
+
 function swapImage(page) {
 var url = new Array();
 url[0]="jpg/about.jpg";
 url[1]="jpg/skills.jpg";
 url[2]="jpg/blog.jpg";
 url[3]="jpg/projects.jpg";
+url[4]="jpg/home.jpg"; // this is for mouseout actions
 // define image locations
 document.getElementById('wrapper').style.backgroundImage = "url("+url[page]+")";
 }
 
-function defaultImage() {
-document.getElementById('wrapper').style.backgroundImage = "url(jpg/home.jpg)";
 
+function fadeIn() {
+ document.getElementById('t').style.backgroundColor = "white";
+ // will make the table white over time
+}
+function fadeOut() {
+// will make the table transparent again, showing the background image
+document.getElementById('t').style.backgroundColor = "transparent";
+}
+function image(page) {
+setTimeout(fadeIn, 500); // this will be called even if the mouse out event occures so i will need a variable to handle that
+setTimeout(function a(){swapImage(page)},500);
+setTimeout(fadeOut, 1000);
 }
