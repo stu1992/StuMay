242bd24b720dae7a723eb5b32731c1eba4965544
Stu <stumay1992@gmail.com>
2014-05-04 18:28:08 +1000

    added timing and delta functions to hover.js

--- www/hover.js
+++ www/hover.js
@@ -1,6 +1,9 @@
+// timing is always on
+setInterval(timing_function,50);
 // global vars
 var activeButton =0;
 var mouseY = 0;
+
 function hover_update(event, hotspot)
 {
 //var x=event.clientX;
@@ -12,9 +15,21 @@ var pos = document.getElementById(hotspot).getBoundingClientRect();
 //alert(pos.top);
 var button = document.getElementById(hotspot);
 var center = 100;
-button.style.top = mouseY;
+button.style.top = mouseY - (pos.bottom - pos.top)/2 - 10; // 10 deals with padding or something ;)
 }
 
 
+function timing_function(){
+   var b1 = document.getElementById("1");
+   var b2 = document.getElementById("2");
+   var b3 = document.getElementById("3");
+   var b4 = document.getElementById("4");
+   var buttons = [b1,b2,b3,b4];
+   for ( var i = 0; i < 4 ; i ++){
+      buttons[i].style.top = 30;
+   }
+}
 
-//setInterval("javascript function",milliseconds);
+function delta(){
+
+}
