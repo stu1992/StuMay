535b31f398428e048f2b3c6e619d9f101f098f8d
Stu <stumay1992@gmail.com>
2014-05-04 21:12:17 +1000

    cleaned up code

--- www/hover.js
+++ www/hover.js
@@ -9,16 +9,11 @@ function hover_update(event, hotspot)
    if(activeButton==5){
       return;
    }
-//var x=event.clientX;
 mouseY=event.clientY;
-//alert("Y: " + mouseY + "button: " + hotspot);
 activeButton = hotspot;
-// now we will get the y position value of button and make it the same as the cursor just for prototyping
 var pos = document.getElementById(hotspot).getBoundingClientRect();
-//alert(pos.top);
 var button = document.getElementById(hotspot);
 }
-
 
 function timing_function(){
    var b1 = document.getElementById("1");
@@ -28,11 +23,11 @@ function timing_function(){
    var buttons = [b1,b2,b3,b4];
    for ( var i = 0; i < 4 ; i ++){
       if(activeButton == i+1){
-      buttons[i].style.top = delta(buttons[i], "mouse");// mouseY - (pos.bottom - pos.top)/2 - 10; // 10 deals with padding or something ;)
+      buttons[i].style.top = delta(buttons[i], "mouse");
       }else if(activeButton == 5){
-      buttons[i].style.top = delta(buttons[i], "floor");// mouseY - (pos.bottom - pos.top)/2 - 10; // 10 deals with padding or something ;)
+      buttons[i].style.top = delta(buttons[i], "floor");
       }else{
-       buttons[i].style.top = delta(buttons[i], "center");// mouseY - (pos.bottom - pos.top)/2 - 10; // 10 deals with padding or something ;)
+       buttons[i].style.top = delta(buttons[i], "center");
    
       }
    }
