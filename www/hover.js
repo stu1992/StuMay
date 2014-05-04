9cf6fb52091d00ec715a9b2eed63145e45c6c3c9
Stu <stumay1992@gmail.com>
2014-05-04 19:32:04 +1000

    have objects returning to center

--- www/hover.js
+++ www/hover.js
@@ -1,5 +1,5 @@
 // timing is always on
-setInterval(timing_function,50);
+setInterval(timing_function,20);
 // global vars
 var activeButton =0;
 var mouseY = 0;
@@ -25,13 +25,18 @@ function timing_function(){
    var b4 = document.getElementById("4");
    var buttons = [b1,b2,b3,b4];
    for ( var i = 0; i < 4 ; i ++){
+      if(activeButton == i+1){
       buttons[i].style.top = delta(buttons[i], "mouse");// mouseY - (pos.bottom - pos.top)/2 - 10; // 10 deals with padding or something ;)
-
+      }else{
+       buttons[i].style.top = delta(buttons[i], "center");// mouseY - (pos.bottom - pos.top)/2 - 10; // 10 deals with padding or something ;)
+   
+      }
    }
 }
 
 function delta(button, relatedObject){
    var roof = 0;
+   var center = 300;
    var floor = 600;
    if(relatedObject == "mouse"){
       var initPosition = (button.getBoundingClientRect().top);
@@ -43,11 +48,21 @@ function delta(button, relatedObject){
             if(delta >= 40){
         return (initPosition+5);
       }else if(delta <= -50){
-         return (initPosition-15);
+         return (initPosition-20);
       }else{
         return mouseY-40; 
       }
-      //return delta/20;
    }
-   //return mouseY-40;
+   else if(relatedObject == "center"){
+      var initPosition = (button.getBoundingClientRect().top);
+      var delta = center - initPosition;
+                  if(delta >= 40){
+        return (initPosition+5);
+      }else if(delta <= -50){
+         return (initPosition-20);
+      }else{
+        return center; 
+      }
+
+   }
 }
