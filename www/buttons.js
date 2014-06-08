6a79a646f6dd06621bb514c2482ab2faef50084d
Stu <stumay1992@gmail.com>
2014-06-08 22:40:47 +1000

    updated all the background images and changed the file format to PNG

--- www/buttons.js
+++ www/buttons.js
@@ -14,11 +14,11 @@ window.location=url[page];
 
 function swapImage(page) {
 var url = new Array();
-url[0]="jpg/about.jpg";
-url[1]="jpg/skills.jpg";
-url[2]="jpg/blog.jpg";
-url[3]="jpg/projects.jpg";
-url[4]="jpg/home.jpg"; // this is for mouseout actions
+url[0]="jpg/about.PNG";
+url[1]="jpg/skills.PNG";
+url[2]="jpg/blog.PNG";
+url[3]="jpg/projects.PNG";
+url[4]="jpg/home.PNG"; // this is for mouseout actions
 // define image locations
 document.getElementById('t').style.backgroundColor="white";
 setTimeout(function(){document.getElementById('wrapper').style.backgroundImage = "url("+url[page]+")"},500); 
