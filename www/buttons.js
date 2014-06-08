81e2169c8b5bb2949d6bef8daf93307cc8e6a818
Stu <stumay1992@gmail.com>
2014-06-08 23:01:44 +1000

    increased the fadeing times for background images

--- www/buttons.js
+++ www/buttons.js
@@ -21,13 +21,13 @@ url[3]="jpg/projects.PNG";
 url[4]="jpg/home.PNG"; // this is for mouseout actions
 // define image locations
 document.getElementById('t').style.backgroundColor="white";
-setTimeout(function(){document.getElementById('wrapper').style.backgroundImage = "url("+url[page]+")"},500); 
+setTimeout(function(){document.getElementById('wrapper').style.backgroundImage = "url("+url[page]+")"},700); 
 var newC = document.getElementById('t');
-setTimeout(function(){newC.style.backgroundColor="transparent"},500);
+setTimeout(function(){newC.style.backgroundColor="transparent"},700);
 }
 
 var init;
 function image(page) {
 clearTimeout(init);
-init = setTimeout(function(){swapImage(page)},1000);
+init = setTimeout(function(){swapImage(page)},1400);
 }
